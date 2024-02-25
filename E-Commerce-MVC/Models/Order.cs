79b3053c291b455593d58bcbe2056fdeff33a3e9
aya378 <youagoda22@gmail.com>
2024-02-25 15:13:39 +0200

    Order&OrderItem classes,navigate properities are commented, you can uncomment them after all classes are implemented.

---
+++ E-Commerce-MVC/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Commerce_MVC.Models
+{
+    public class Order
+    {
+        public int OrderId { set; get; }
+        public DateTime OrderDate { set; get; }
+        [Column(TypeName ="money")]
+        public decimal OrderPrice { set; get; }
+        //[ForeignKey("customer")]
+        public int CustomeCusto { set; get; }
+        //[ForeignKey("payment")]
+        public int PaymentPayme { set; get; }
+        //[ForeignKey("customer")]
+        public int ShipmentShipm { set; get; }
+        //public virtual Shipment shipm  { set; get; }
+        //public virtual Payment payment  { set; get; }
+        //public virtual Customer customer  { set; get; }
+        public virtual List<OrderItem> OrderItems { set; get; }=new List<OrderItem>();
+    }
+}
---
+++ E-Commerce-MVC/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Commerce_MVC.Models
+{
+    
+    public class OrderItem
+    {
+        //[PrimaryKey("OrderItemId", "OederOrderI")]
+        public int OrderItemId { get; set; }
+        public int Quantity { get; set; }
+        [Column(TypeName ="money")]
+        public decimal Price { get; set; }
+        //[ForeignKey("product")]
+        public int ProductProd { get; set; }
+        public int OederOrderI { get; set; }
+        //public virtual Product product  {  get; set; }
+        //public virtual Order order{get;set;}
+    }
+}
