37be38c0d7bce522d3ed1eab1cfa5ecc2de2bfb1
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-02-25 16:02:44 +0200

    Update OrderItem.cs
    Made foreign keys nullable and changed some properties names

--- E-Commerce-MVC/Models/OrderItem.cs
+++ E-Commerce-MVC/Models/OrderItem.cs
@@ -6,13 +6,15 @@ namespace E_Commerce_MVC.Models
     public class OrderItem
     {
         //[PrimaryKey("OrderItemId", "OederOrderI")]
-        public int OrderItemId { get; set; }
+        public int Id { get; set; }
         public int Quantity { get; set; }
         [Column(TypeName ="money")]
         public decimal Price { get; set; }
         //[ForeignKey("product")]
-        public int ProductProd { get; set; }
-        public int OederOrderI { get; set; }
+        public int? ProductId { get; set; }
+        public int? OrderId { get; set; }
+
+        // Navigation Properties
         //public virtual Product product  {  get; set; }
         //public virtual Order order{get;set;}
     }
