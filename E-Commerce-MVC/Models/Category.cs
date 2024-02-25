877c8da6ace29ff8a0c04642945ce0964a4a6f93
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-02-25 21:47:04 +0200

    Category and Product Models---Done

---
+++ E-Commerce-MVC/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace E_Commerce_MVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
