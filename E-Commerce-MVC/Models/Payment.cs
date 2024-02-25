7da67f661bc224152de6995717af870037a5f3b0
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-02-25 13:43:52 +0200

    Added cart and payment models

---
+++ E-Commerce-MVC/Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Commerce_MVC.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Method { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+        public int? CustomerId { get; set; }
+
+    }
+}
