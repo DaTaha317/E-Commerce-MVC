7cfae90d80cc4cea96e99e8ac1ec91854635436c
Mostafa-Elzareef79 <mostafaelzareef79@gmail.com>
2024-02-25 13:35:18 +0200

    added Shipment and Customer Models and make a Enum for Shipment Stetus

---
+++ E-Commerce-MVC/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace E_Commerce_MVC.Models
+{
+    public class Customer
+    {
+        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime JoinDate { get; set; }
+  
+
+    }
+}
---
+++ E-Commerce-MVC/Models/Shipment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_MVC.Models
+{
+   
+    public class Shipment
+    {
+        
+        public int Id { get; set; }
+        public DateTime ShipmentDate { get;set; }
+        public string Address { get;set; }
+        public string City { get; set; }  
+        public string State { get;set; } 
+        
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public ShipmentStatus Status { get; set; }
+        public int? CustomerId { get; set; }
+        
+
+    }
+}
