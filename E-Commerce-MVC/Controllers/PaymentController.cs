1910f0b257abfde82126da6298658c47923725e4
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-02-27 13:28:06 +0200

    Shipment and Payment Controllers, Views not created yet..

---
+++ E-Commerce-MVC/Controllers/PaymentController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_MVC.Interfaces;
+using E_Commerce_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_MVC.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private IPaymentRepo context;
+
+        public PaymentController(IPaymentRepo context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Payment> payments = context.GetAll();
+            return View(payments);
+        }
+    }
+}
---
+++ E-Commerce-MVC/Controllers/ShipmentController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_MVC.Interfaces;
+using E_Commerce_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_MVC.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        private IShipmentRepo context;
+
+        public ShipmentController(IShipmentRepo context)
+        {
+            this.context = context;  
+        }
+        public IActionResult Index()
+        {
+            List<Shipment> shipments = context.GetAll();
+            return View(shipments);
+        }
+    }
+}
