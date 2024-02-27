9586c04b550571b6e2a92da1ec7f48576d89784b
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-02-27 18:22:14 +0200

    Changed shipentStatus Enum and added migration v3

--- E-Commerce-MVC/Models/OrderItem.cs
+++ E-Commerce-MVC/Models/OrderItem.cs
@@ -5,7 +5,6 @@ namespace E_Commerce_MVC.Models
     
     public class OrderItem
     {
-        //[PrimaryKey("OrderItemId", "OederOrderI")]
         public int Id { get; set; }
         public int Quantity { get; set; }
         [Column(TypeName ="money")]
