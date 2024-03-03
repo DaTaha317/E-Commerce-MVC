7fdaffe35732c39fbcfdda270efa64039c0a1274
Ahmed Taha <149804685+DaTaha317@users.noreply.github.com>
2024-03-03 20:23:33 +0200

    Added Services and Contact us pages

---
+++ E-Commerce-MVC/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_MVC.Controllers
+{
+	public class ContactController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ E-Commerce-MVC/Controllers/ViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_MVC.Controllers
+{
+	public class ViewController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
